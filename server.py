3dc7df5b9094cfebf9674c318ba4b6f7a4238a50
Davide Andreolli <s-kevin.cressotti@istitutopilati.it>
2021-04-19 11:58:25 +0200

    Prime versioni di test del server

---
+++ server.py
@@ -0,0 +1,26 @@
+import flask
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    with open("index.html") as f:
+        return str(f.read())
+    
+@app.route('/index.css', methods=['GET'])
+def css():
+    with open("index.css") as f:
+        return str(f.read())
+
+@app.route('/index.js', methods=['GET'])
+def js():
+    with open("index.js") as f:
+        return str(f.read())
+
+@app.route("/prenota", methods=['POST'])
+def prenota():
+    print("Arrived")
+    return ""
+
+app.run()
