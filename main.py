fa74293e76fea0e8da504d2d5275015d062748a1
Davide Andreolli <dvd2003333@gmail.com>
2021-04-22 16:21:24 +0200

    Sistemato un problema sul main

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import flask
 from server import server_routes, database_utilities, server_utilities, responses
 from utilities import dates, control_panel
 
-control_panel.create_xlsx_file()
-
 # Setup server
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
