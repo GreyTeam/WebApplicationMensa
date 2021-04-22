0107a18574f2154a7013f7bd9d12b0243ac6a56b
Davide Andreolli <dvd2003333@gmail.com>
2021-04-22 11:57:15 +0200

    Creato lo script per il setup dei file del database

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+os.mkdir("data")
+os.chdir("data")
+
+json.dump({
+    "totale": 0,
+    "users": []
+}, open("users.json", "w"), indent=4)
+
+json.dump({
+    "totale": 0,
+    "prenotazioni": []
+}, open("prenotazioni.json", "w"), indent=4)
