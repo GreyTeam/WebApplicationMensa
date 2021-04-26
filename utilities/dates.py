2dc1ad638b605a903792b2cfb79fba50ae06ab82
Davide Andreolli <dvd2003333@gmail.com>
2021-04-26 10:26:17 +0200

    Creato l'algoritmo che esclude le date festive

--- utilities/dates.py
+++ utilities/dates.py
@@ -1,4 +1,5 @@
-from datetime import datetime, timedelta
+import datetime
+from server.database_utilities import *
 
 days_name = [
     "Lun", "Mar", "Mer", "Gio", "Ven", "Sab", "Dom"
@@ -9,29 +10,48 @@ months_name = [
     "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"
 ]
 
-def get_next_day(delta):
-    date = datetime.today() + timedelta(days=delta)
-    return {
-        "text": "{0} {1} {2}".format(days_name[date.weekday()], str(date.day), months_name[date.month - 1]),
-        "value": date.strftime("%d/%m")
-    }
+def get_next_day(delta, year=True):
+    date = datetime.datetime.now() + datetime.timedelta(days=delta)
+    if not year:
+        return date.strftime("%d/%m/%Y")
+    else:
+        return {
+            "text": "{0} {1} {2}".format(days_name[date.weekday()], str(date.day), months_name[date.month - 1]),
+            "value": date.strftime("%d/%m")
+        }
+
+def is_holiday(date):
+    db = load_db(dates_file_path)
+    day = datetime.datetime.strptime(date, "%d/%m/%Y")
+    print(day)
+    for date_db in db:
+        start = datetime.datetime.strptime(date_db["from"], "%d/%m/%Y")
+        end = datetime.datetime.strptime(date_db["to"], "%d/%m/%Y")
+        if start <= day <= end:
+            print(True)
+            return True
+    print(False)
+    return False
 
 def create_days_list():
     days = []
     delta = 0
     for day in range(8):
+        print(day)
         day_value = get_next_day(day + delta)
-        day_to_string = day_value["value"] + f"/{str(datetime.now().year)}"
-        if days_name[datetime.strptime(day_to_string, "%d/%m/%Y").weekday()] == "Sab":
+        day_to_string = day_value["value"] + f"/{str(datetime.datetime.now().year)}"
+        if days_name[datetime.datetime.strptime(day_to_string, "%d/%m/%Y").weekday()] == "Sab":
             delta += 1
         else:
-            days.append(get_next_day(day + delta))
+            if not is_holiday(get_next_day(day + delta, year=False)):
+                print(day_value)
+                days.append(day_value)
     return days
 
 def verify_date(date):
-    list = create_days_list()
-    print(list)
-    for d in list:
+    d_list = create_days_list()
+    print(d_list)
+    for d in d_list:
         if d["value"] == date:
             return True
     return False
