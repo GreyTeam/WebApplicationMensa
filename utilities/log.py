283da04c036e172f51aed618420e54f9bd04ae92
Davide Andreolli <dvd2003333@gmail.com>
2021-04-26 08:20:11 +0200

    Risolto un problema di sintassi

--- utilities/log.py
+++ utilities/log.py
@@ -8,7 +8,7 @@ def log(l_type, message, info):
         "type": l_type,
         "message": message,
         "info": info,
-        "timestamp" = datetime.now().timestamp()
+        "timestamp": datetime.now().timestamp()
     })
     save_db(db, log_file_path)
     
