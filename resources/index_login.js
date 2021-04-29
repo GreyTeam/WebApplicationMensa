37e35ef2683090caed7d9d4b4ab4b322457046b2
Davide Andreolli <dvd2003333@gmail.com>
2021-04-29 08:26:50 +0200

    Risolto il problema di merge

--- resources/index_login.js
+++ resources/index_login.js
@@ -1,17 +1,4 @@
-var googleUser = {};
-  var startApp = function() {
-    gapi.load('auth2', function(){
-      // Retrieve the singleton for the GoogleAuth library and set up the client.
-      auth2 = gapi.auth2.init({
-        client_id: 'YOUR_CLIENT_ID.apps.googleusercontent.com',
-        cookiepolicy: 'single_host_origin',
-        // Request scopes in addition to 'profile' and 'email'
-        //scope: 'additional_scope'
-      });
-      attachSignin(document.getElementById('customBtn'));
-    });
-  };
-
+function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     var id_token = googleUser.getAuthResponse().id_token;
 	
